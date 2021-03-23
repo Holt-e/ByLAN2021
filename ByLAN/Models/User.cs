f333c3ebf7e3601a9770929979c3fba6659e61b1
Holt-e <56269370+Holt-e@users.noreply.github.com>
2021-03-23 22:33:53 +0100

    Initial commit

---
+++ ByLAN/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ByLAN.Models
+{
+    public class User
+    {
+        public String Username { get; set; }
+
+        public String Hashedpassword { get; set; }
+
+        public Boolean Admin { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
